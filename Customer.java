51937c7d32fdaa0dbe6295635762e59cce89defb
Jason Evangelista <>
2019-08-05 17:08:36 +0800

    add regionDetails method

--- Customer.java
+++ Customer.java
@@ -13,6 +13,51 @@ public class Customer {
         this.parcel = parcel;
     }
 
+     // /**
+    //  * This method determines the number of delivery days of the parcel and
+    //  * the service fee depending on the region.
+    //  */
+    // public void addRegionDetails(){
+    //     if(this.delRegion.equalsIgnoreCase("MML")){
+    //         this.deliveryDays = 2;
+    //         this.serviceFee = 50;
+    //     }
+    //     else if(this.delRegion.equalsIgnoreCase("LUZ")){
+    //         this.deliveryDays = 3;
+    //         this.serviceFee = 100;
+    //     }
+    //     else if(this.delRegion.equalsIgnoreCase("VIS")){
+    //         this.deliveryDays = 5;
+    //         if(this.totalVolume > this.totalWeight){
+    //             if(this.totalVolume * 0.10 > 1000)
+    //                 this.serviceFee = this.totalVolume * 0.10;
+    //             else
+    //                 this.serviceFee = 1000;
+    //         }
+    //         else{
+    //             if(this.totalWeight * 0.10 > 1000)
+    //                 this.serviceFee = this.totalWeight * 0.10;
+    //             else
+    //                 this.serviceFee = 1000;
+    //         }
+    //     }
+    //     else if(this.delRegion.equalsIgnoreCase("MIN")){
+    //         this.deliveryDays = 8;
+    //         if(this.totalVolume > this.totalWeight){
+    //             if(this.totalVolume * 0.25 > 3000)
+    //                 this.serviceFee = this.totalVolume * 0.25;
+    //             else
+    //                 this.serviceFee = 3000;
+    //         }
+    //         else{
+    //             if(this.totalWeight * 0.25 > 3000)
+    //                 this.serviceFee = this.totalWeight * 0.25;
+    //             else
+    //                 this.serviceFee = 3000;
+    //         }
+    //     }
+    // }
+
     /**
      * @return the recipientName
      */
